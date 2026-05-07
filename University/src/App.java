eaef7738374b406bafccb507a8671ef6b809518f
David-disini <christomoniaga@gmail.com>
2026-05-07 21:07:32 +0700

    Menu awal

---
+++ University/src/App.java
@@ -0,0 +1,31 @@
+import java.util.*;;
+public class App {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println("+================+");
+        System.out.println(" Welcome to ISTTS");
+        System.out.println("+================+");
+        System.out.println();
+        System.out.println("Interaksi yang dapat dipilih:");
+        System.out.println("1. Informasi data \n2. Jadwal Kelas \n3. List Dosen \n4. Map Kampus");
+        System.out.println(">> ");
+        int pilihanMenu = scanner.nextInt();
+        scanner.nextLine();
+        switch (pilihanMenu) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+                break;
+        }
+    }
+}
